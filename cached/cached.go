b57dfa8076b0384561b17a036e4ea7de1a45377f
Neil Wilson <neil@aldur.co.uk>
2020-02-26 15:46:38 +0000

    Cache Server and ServerGroup

--- cached/cached.go
+++ cached/cached.go
@@ -2,17 +2,62 @@ package cached
 
 import (
 	"net/http"
+	"time"
 
 	brightbox "github.com/brightbox/gobrightbox"
+	cache "github.com/patrickmn/go-cache"
+	"k8s.io/klog"
+)
+
+const (
+	expirationTime = 5 * time.Second
+	purgeTime      = 30 * time.Second
 )
 
 // Client is a cached brightbox Client
 type Client struct {
+	clientCache *cache.Cache
 	brightbox.Client
 }
 
 // NewClient creates and returns a cached Client
 func NewClient(url string, account string, httpClient *http.Client) (*Client, error) {
 	cl, err := brightbox.NewClient(url, account, httpClient)
-	return &Client{*cl}, err
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		clientCache: cache.New(expirationTime, purgeTime),
+		Client:      *cl,
+	}, err
+}
+
+//Server fetches a server by id
+func (c *Client) Server(identifier string) (*brightbox.Server, error) {
+	if cachedServer, found := c.clientCache.Get(identifier); found {
+		klog.V(4).Infof("Cache hit %q", identifier)
+		return cachedServer.(*brightbox.Server), nil
+	}
+	server, err := c.Client.Server(identifier)
+	if err != nil {
+		return nil, err
+	}
+	klog.V(4).Infof("Cacheing %q", identifier)
+	c.clientCache.Set(identifier, server, cache.DefaultExpiration)
+	return server, nil
+}
+
+//ServerGroup fetches a server group by id
+func (c *Client) ServerGroup(identifier string) (*brightbox.ServerGroup, error) {
+	if cachedServerGroup, found := c.clientCache.Get(identifier); found {
+		klog.V(4).Infof("Cache hit %q", identifier)
+		return cachedServerGroup.(*brightbox.ServerGroup), nil
+	}
+	serverGroup, err := c.Client.ServerGroup(identifier)
+	if err != nil {
+		return nil, err
+	}
+	klog.V(4).Infof("Cacheing %q", identifier)
+	c.clientCache.Set(identifier, serverGroup, cache.DefaultExpiration)
+	return serverGroup, nil
 }
