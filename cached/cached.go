11bffe754f48f185757c057454397efdc5fde87f
Neil Wilson <neil@aldur.co.uk>
2020-02-26 14:02:11 +0000

    Split out client creation code
    Move the auth functions to a separate file and run through the
    cache module with a straight passthrough implementation

--- cached/cached.go
+++ cached/cached.go
@@ -1,10 +1,18 @@
 package cached
 
 import (
+	"net/http"
+
 	brightbox "github.com/brightbox/gobrightbox"
 )
 
 // Client is a cached brightbox Client
 type Client struct {
 	brightbox.Client
+}
+
+// NewClient creates and returns a cached Client
+func NewClient(url string, account string, httpClient *http.Client) (*Client, error) {
+	cl, err := brightbox.NewClient(url, account, httpClient)
+	return &Client{*cl}, err
 }
